1aa363e819ec23742785ac15e6fad450594f366f
Pietro di Paulo <pietrodipaulo4@gmail.com>
2021-02-10 10:06:11 -0300

    Adicionar arquivos de projeto.

---
+++ Entity-Asp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Entity_Asp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
